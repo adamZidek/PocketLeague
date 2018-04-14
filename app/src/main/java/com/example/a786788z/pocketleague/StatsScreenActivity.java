a9b45a632805b4fff6d36d9cfb4afd2600f80ba7
adamZidek <adam.zidek40@gmail.com>
2018-04-14 17:30:40 -0400

    n

--- app/src/main/java/com/example/a786788z/pocketleague/StatsScreenActivity.java
+++ app/src/main/java/com/example/a786788z/pocketleague/StatsScreenActivity.java
@@ -1,5 +1,7 @@
 package com.example.a786788z.pocketleague;
 
+import android.annotation.SuppressLint;
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -11,6 +13,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import java.io.Serializable;
@@ -21,34 +24,36 @@ import static android.widget.Toast.LENGTH_LONG;
  * Created by A786788Z on 4/7/2018.
  */
 
-public class StatsScreenActivity extends AppCompatActivity {
 
+//https://stackoverflow.com/questions/12131025/android-preventing-webview-reload-on-rotate
+
+public class StatsScreenActivity extends AppCompatActivity {
 
     private Intent intent;
     private BottomNavigationView navigationView;
     private int myView;
+    private Bundle webView_bundle;
+    private WebView webView;
 
     @RequiresApi(api = Build.VERSION_CODES.ECLAIR)
     protected void onCreate(Bundle savedInstanceState) {
+        Toast.makeText(getBaseContext(), "OnCreate", Toast.LENGTH_LONG).show();
         overridePendingTransition(0, 0);
         super.onCreate(savedInstanceState);
-        myView = R.layout.stats_screen;
-        setContentView(myView);
-        WebView webView;
-        webView = (WebView) findViewById(R.id.newsWebView);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
+
 
         if(savedInstanceState != null)
         {
-            myView = savedInstanceState.getInt("ourView");
+            webView.restoreState(savedInstanceState);
         }
         else
         {
+            myView = R.layout.stats_screen;
+            setContentView(myView);
+            webView = findViewById(R.id.newsWebView);
+            webView.setWebViewClient(new WebViewClient());
             webView.loadUrl("https://rocketleaguestats.com/profile");
         }
-
-
 
         //Set navigation item to be selected corresponding to current activity
         navigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
@@ -82,20 +87,32 @@ public class StatsScreenActivity extends AppCompatActivity {
                 return true;
             }
         });
+
     }
-    protected void onSaveInstanceState(Bundle outState) {
-
-        super.onSaveInstanceState(outState);
-
-        outState.putInt("ourView", myView);
-    }
-
-    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    /*
+    protected void onResume()
     {
-        super.onRestoreInstanceState(savedInstanceState);
-        myView = savedInstanceState.getInt("ourView");
+        Toast.makeText(getBaseContext(), "OnResume", Toast.LENGTH_LONG).show();
 
+        if (webView_bundle != null) {
+            webView.restoreState(webView_bundle);
+        }
+
+        super.onResume();
     }
 
+    protected void onPause()
+    {
+        super.onPause();
+        Toast.makeText(getBaseContext(), "OnPause", Toast.LENGTH_LONG).show();
+        webView_bundle = new Bundle();
+        webView.saveState(webView_bundle);
+    }
+ */
+    @SuppressLint("MissingSuperCall")
+    protected void onSaveInstanceState(Bundle outState) {
+        Toast.makeText(getBaseContext(), "OnSAVE", Toast.LENGTH_LONG).show();
+        webView.saveState(outState);
+    }
 
 }
